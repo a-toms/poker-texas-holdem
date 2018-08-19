6a5a216b9366ce84cd1864923dd283dd0c12a1d2
A <1239@pm.me>
2018-08-20 00:27:28 +0100

    All basic game checks are working!

--- connect_4.py
+++ connect_4.py
@@ -8,41 +8,46 @@ logging.basicConfig(level=logging.INFO)
 BLANK_BOARD_VALUE = '-'
 
 
-def is_sublist_in_sequence(seq, sublist):
-    n = len(sublist)
-    return sublist in (seq[i:i+n] for i in range(len(seq) + 1 - n))
+def is_shorter_sequence_in_longer_sequence(shorter_sequence, longer_sequence):
+    n = len(shorter_sequence)
+    return shorter_sequence in (
+        longer_sequence[i:i+n] for i in range(len(longer_sequence) + 1 - n))
 
 
-def horizontal_four_check(seq, segment):
-    return is_sublist_in_sequence(seq, segment)
+def horizontal_four_check(win_sequence, full_sequence):
+    return is_shorter_sequence_in_longer_sequence(win_sequence, full_sequence)
 
 
-def vertical_four_check(seq, column, board):
-    column = [row[column] for row in board]
-    return is_sublist_in_sequence(seq, column)
+def vertical_four_check(board, column, win_sequence):
+    column_values = []
+    for row in board:
+        column_values.append(row[column])
+    logging.info(f"vertical column_values = {column_values}")
+    return is_shorter_sequence_in_longer_sequence(win_sequence, column_values)
 
 
 def negative_diagonal_four_check(row, column, board, win_sequence):
     negative_diagonal = []
     for i in range(-3, 4):
-        position = board[row + i][column + i]
-        logging.info('Cell position = %s' % position)
         try:
-            negative_diagonal.append((position))
+            position = board[row + i][column + i]  # todo: Consider using the suppress module here
+            negative_diagonal.append(position)
         except IndexError:
             pass
-    logging.info('negative_diagonal = %s' % negative_diagonal)
-    return is_sublist_in_sequence(negative_diagonal, win_sequence)
+    logging.info(f"negative diagonal values = {negative_diagonal}")
+    return is_shorter_sequence_in_longer_sequence(win_sequence, negative_diagonal)
 
 
-def positive_diagonal_four_check(board_p, board, sublist_p): # todo: improve argument names
+def positive_diagonal_four_check(row, column, board, win_sequence):
     positive_diagonal = []
-    for i in range(-3, 4):
+    for i in range(3, -4, -1):
         try:
-            positive_diagonal.append(board[board_p + i][sublist_p + i])
+            position = board[row - i][column + i]
+            positive_diagonal.append(position)
         except IndexError:
             pass
-    return is_sublist_in_sequence(positive_diagonal, win_sequence)
+    logging.info(f"positive_diagonal = {positive_diagonal}")
+    return is_shorter_sequence_in_longer_sequence(win_sequence, positive_diagonal)
 
 
 def generate_board(size):
@@ -61,23 +66,23 @@ def get_player_2_token():
     return player_2_token
 
 
-def show_board(board): # todo: show the board with numbered row and column displays
+def show_board(board):  # todo: show the board with numbered row and column displays
     pprint.pprint(board)
 
 
 def choose_position_to_place_counter(player_number, board):
     while True:
-        command = str(input(
-            f"Player {str(player_number)}, please state the row and column" +
-            "to place your token"))
-        row, column = map(int, command.split())
         try:
+            command = str(input(
+                f"Player {str(player_number)}, please state the row and column" +
+                "to place your token"))
+            row, column = map(int, command.split())
             if board[row][column] != BLANK_BOARD_VALUE:
                 print("That position is occupied. " +
                       "Please enter a different row and column")
                 continue
             else:
-                return (int(row), int(column))
+                return row, column
         except (ValueError, IndexError) as e:
             print(f"The program suffered an error: {e}. " +
                   f"Please re-enter the row and column")
@@ -87,35 +92,29 @@ def place_counter(board, player_token, row, column):
     board[row][column] = player_token
 
 
-
-def engine():
+def engine():  # todo: build game!
     board = generate_board(8)
-    pprint.pprint(board)
-    #player_1_token = get_player_1_token()
+    show_board(board)
+    # player_1_token = get_player_1_token()
     p1_token = 'X'
     p1_win_seq = [p1_token for i in range(4)]
-    #player_2_token = get_player_2_token()
+    # player_2_token = get_player_2_token()
     p2_token = 'O'
-    is_game_complete = False
-    while is_game_complete == False:
+    while True:
         x, y = choose_position_to_place_counter(1, board)
         place_counter(board, p1_token, x, y)
         show_board(board)
-        is_game_complete = horizontal_four_check(board[x], p1_win_seq)
-        is_game_complete = negative_diagonal_four_check(x, y, board, p1_win_seq)
-        print(board[x])
-        #vertical_four_check(player_1_token * 4, (), sublist, board)
-        #x, y = choose_position_to_place_counter(2)
-        #place_counter(p2_token, x, y)
-        #how_board(board)
+        if horizontal_four_check(p1_win_seq, board[x]) is True:
+            break
+        if negative_diagonal_four_check(x, y, board, p1_win_seq) is True:
+            break
+        if vertical_four_check(board, y, p1_win_seq) is True:
+            break
+        if positive_diagonal_four_check(x, y, board, p1_win_seq) is True:
+            break
     print('Player won')
 
-    segment = ['x', 'x']
-    sequence = ['o', '-', 'x', 'x', 'o']
-    print(is_sublist_in_sequence(sequence, segment))
 
+engine()
 
-# engine()
-
-
-# After MVP, consider how to use classes to greater effect to run the game.
+# todo: consider refactoring the program to use classes
--- test_connect_4.py
+++ test_connect_4.py
@@ -1,15 +1,15 @@
 import unittest
 import connect_4
-import pprint
 import logging
 logging.basicConfig(level=logging.INFO)
 
-class SimpleTest(unittest.TestCase):
+
+class TestSetup(unittest.TestCase):
 
     test_board = connect_4.generate_board(8)
     test_token = 'T'
 
-    def return_win_sequence(self):
+    def generate_win_sequence(self):
         return [self.test_token for i in range(4)]
 
     def test_board_creation(self):
@@ -18,28 +18,51 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(len(board), n)
         self.assertEqual(len(board[0]), n)
 
+
+class TestHorizontalAndVerticalGameSuccess(TestSetup):
+
     def test_horizontal_check(self):
-        test_win_sequence = self.return_win_sequence()
+        test_win_sequence = self.generate_win_sequence()
+        row = 5
         for i in range(4):
-            connect_4.place_counter(self.test_board, self.test_token, 0, i)
+            connect_4.place_counter(self.test_board, self.test_token, row, i)
         self.assertTrue(
             connect_4.horizontal_four_check(
-                self.test_board[0], test_win_sequence
-            ))
+                test_win_sequence, self.test_board[row]))
+
+    def test_vertical_check(self):
+        test_win_sequence = self.generate_win_sequence()
+        column = 6
+        for i in range(4):
+            connect_4.place_counter(
+                self.test_board, self.test_token, i, column)
+        self.assertTrue(
+            connect_4.vertical_four_check(
+                self.test_board, column, test_win_sequence))
+
+
+class TestNegativeDiagonalGameSuccess(TestSetup):
 
     def test_negative_diagonal_check(self):
         for i in range(-4, 2):
             connect_4.place_counter(self.test_board, self.test_token, i, i)
-        pprint.pprint(self.test_board)
-        test_win_sequence = self.return_win_sequence()
+        test_win_sequence = self.generate_win_sequence()
         self.assertTrue(connect_4.negative_diagonal_four_check(
             4, 4, self.test_board, test_win_sequence))
 
 
-# todo: write tests for the positive diagonal check and the vertical check
-
-
-
+class TestPositiveDiagonalGameSuccess(unittest.TestCase):
+    def generate_win_sequence(self):
+        return [self.test_token for i in range(4)]
+    test_board = connect_4.generate_board(8)
+    test_token = 'T'
+    def test_positive_diagonal_check(self):
+        connect_4.place_counter(self.test_board, 'T', 3, 0)
+        connect_4.place_counter(self.test_board, 'T', 2, 1)
+        connect_4.place_counter(self.test_board, 'T', 1, 2)
+        connect_4.place_counter(self.test_board, 'T', 0, 3)
+        self.assertTrue(connect_4.positive_diagonal_four_check(
+            2, 1, self.test_board, self.generate_win_sequence()))
 
 
 
