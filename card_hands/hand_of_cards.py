bf14172083630dcc28312ee931f9fca83e366f18
A <1239@pm.me>
2018-10-01 00:00:31 +0100

    Adding card_hands.

---
+++ card_hands/hand_of_cards.py
@@ -0,0 +1,95 @@
+import random
+
+
+class GetCards:
+    hand = []
+    suites = ('H', 'D', 'S', 'C')
+
+    def pick_card(self):
+        card_number = random.randint(1, 13)
+        suite = random.choice(self.suites)
+        if (card_number, suite) in self.hand:
+            self.pick_card()
+        else:
+            self.hand.append((card_number, suite))
+
+    def pick_hand_of_cards(self, number_of_hand_cards):
+        for i in range(number_of_hand_cards):
+            self.pick_card()
+
+
+class GetHandRankings:
+
+    def get_pairs(self, hand):
+        card_numbers = [card[0] for card in hand]
+        number_of_cards_in_a_pair = 2
+        pairs = ()
+        for card_number in card_numbers:
+            if card_numbers.count(card_number) == number_of_cards_in_a_pair:
+                pairs += (card_number,)
+        if pairs != ():
+            return tuple(set(pairs))
+
+
+    def get_two_pairs(self, hand):
+        if len(set(self.get_pairs(hand))) == 2:
+            return set(self.get_pairs(hand))
+
+    def get_three_of_a_kind(self, hand):
+        card_numbers = [card[0] for card in hand]
+        triples = ()
+        for card_number in card_numbers:
+            if card_numbers.count(card_number) == 3:
+                triples += (card_number,)
+        if triples != ():
+            return triples
+
+    def get_four_of_a_kind(self, hand):
+        card_numbers = [card[0] for card in hand]
+        quads = ()
+        for card_number in card_numbers:
+            if card_numbers.count(card_number) == 4:
+                quads += (card_number,)
+        if quads != ():
+            return quads
+
+    def get_full_house(self, hand):
+        card_numbers = tuple([card[0] for card in hand])
+        if self.get_three_of_a_kind(hand) is not None:
+            if self.get_pairs(hand):
+                return tuple(card_numbers)
+
+    # TODO: Cont
+    def get_straight(self, hand): # Note that aces, i.e., 1s, are high and low
+        pass
+
+
+    def get_flush(self, hand):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    new_hand = GetCards()
+    new_hand.pick_hand_of_cards(52)
+    test_hand = [
+        (4, 'C'), (2, 'C'), (5, 'S'),
+        (2, 'S'), (4, 'D')
+    ]
+    hand_ranker = GetHandRankings()
+    results = hand_ranker.get_two_pairs(test_hand)
+    print(results)
+
+
+
+
+
---
+++ card_hands/test_hand_of_cards.py
@@ -0,0 +1,111 @@
+from hand_of_cards import GetCards, GetHandRankings
+
+
+def test_no_duplicates():
+    new_hand = GetCards()
+    new_hand.pick_hand_of_cards(52)
+    test_array_1 = new_hand.hand
+    test_array_2 = set(new_hand.hand)
+    assert len(test_array_1) == len(test_array_2)
+
+
+def test_get_pairs():
+    hand_ranker = GetHandRankings()
+    test_hand_pair = [
+        (1, 'C'), (2, 'C'), (4, 'S'),
+        (3, 'S'), (4, 'D')
+    ]
+    test_hand_no_pair = [
+        (1, 'C'), (10, 'C'), (4, 'S'),
+        (3, 'S'), (9, 'D')
+    ]
+    assert len(hand_ranker.get_pairs(test_hand_pair)) == 1
+    assert hand_ranker.get_pairs(test_hand_no_pair) == None
+
+
+def test_get_two_pairs():
+    hand_ranker = GetHandRankings()
+    test_hand_two_pairs = [
+        (1, 'C'), (2, 'C'), (4, 'S'),
+        (2, 'S'), (4, 'D')
+    ]
+    test_hand_no_two_pairs = [
+        (2, 'C'), (10, 'C'), (4, 'S'),
+        (3, 'S'), (4, 'D')
+    ]
+    assert len(hand_ranker.get_two_pairs(test_hand_two_pairs)) == 2
+    assert hand_ranker.get_two_pairs(test_hand_no_two_pairs) == None
+
+
+def test_three_of_a_kind():
+    hand_ranker = GetHandRankings()
+    test_hand_no_triples = [
+        (10, 'C'), (2, 'C'), (10, 'S'),
+        (3, 'S'), (9, 'D')
+    ]
+    test_hand_triples = [
+        (10, 'C'), (2, 'C'), (10, 'S'),
+        (3, 'S'), (10, 'D')
+    ]
+    assert len(hand_ranker.get_three_of_a_kind(test_hand_triples)) == 3
+    assert hand_ranker.get_three_of_a_kind(test_hand_no_triples) == None
+
+
+def test_four_of_a_kind():
+    hand_ranker = GetHandRankings()
+    test_hand_no_quads = [
+        (10, 'C'), (2, 'C'), (10, 'S'),
+        (8, 'H'), (10, 'D')
+    ]
+    test_hand_quads = [
+        (10, 'C'), (2, 'C'), (10, 'S'),
+        (10, 'H'), (10, 'D')
+    ]
+    assert len(hand_ranker.get_four_of_a_kind(test_hand_quads)) == 4
+    assert hand_ranker.get_four_of_a_kind(test_hand_no_quads) == None
+
+
+def test_full_house():
+    hand_ranker = GetHandRankings()
+    test_full_house = [
+        (10, 'C'), (10, 'D'), (10, 'S'),
+        (8, 'H'), (8, 'D')
+    ]
+    test_no_full_house = [
+        (10, 'C'), (2, 'C'), (10, 'S'),
+        (10, 'H'), (1, 'D')
+    ]
+    assert hand_ranker.get_full_house(test_full_house) == (10, 10, 10, 8, 8)
+    assert hand_ranker.get_full_house(test_no_full_house) is None
+
+
+def test_straight():
+    test_straight = [
+        (1, 'C'), (2, 'D'), (3, 'S'),
+        (4, 'H'), (5, 'D')
+    ]
+    test_no_straight = [
+        (10, 'C'), (2, 'C'), (10, 'S'),
+        (3, 'H'), (1, 'D')
+    ]
+
+
+def test_flush():
+    test_flush = [
+        (10, 'C'), (2, 'C'), (5, 'C'),
+        (3, 'C'), (1, 'C')
+    ]
+    test_no_flush = [
+        (10, 'C'), (4, 'C'), (10, 'S'),
+        (8, 'C'), (3, 'C')
+    ]
+
+def test_straight_flush():
+    test_straight_flush = [
+        (1, 'C'), (2, 'C'), (5, 'C'),
+        (3, 'C'), (4, 'C')
+    ]
+    test_no_straight_flush = [
+        (10, 'C'), (2, 'C'), (5, 'C'),
+        (3, 'C'), (1, 'C')
+    ]
